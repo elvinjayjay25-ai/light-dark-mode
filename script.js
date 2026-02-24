b6b0dd76e91ef58139a4c2aedd44300b3e0a20a2
elvinjayjay25-ai <elvin.jayjay25@gmail.com>
2026-02-23 18:28:32 -0600

    updated index.html and added script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+const themeSwitch = document.getElementById("theme-switch");
+const body = document.body;
+
+//Load saved theme
+if (localStorage.getItem("theme") === "dark") {
+    body.classList.add("dark");
+    themeSwitch.checked = true;
+}
+
+// Toggle theme on switch change
+themeSwitch.addEventListener("change", () => {
+    if (themeSwitch.checked) {
+        body.classList.add("dark")
+        localStorage.setItem("theme", "dark");
+    } else {
+        body.classList.remove("dark");
+        localStorage.setItem("theme", "light");
+    }
+});
