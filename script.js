cff96bb8ca61653df1aa59ef83bf8cc5b3a8cef2
elvinjayjay25-ai <elvin.jayjay25@gmail.com>
2026-02-23 18:44:10 -0600

    updated script.js

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 const themeSwitch = document.getElementById("theme-switch");
 const body = document.body;
 
-//Load saved theme
+// load saved theme
 if (localStorage.getItem("theme") === "dark") {
     body.classList.add("dark");
     themeSwitch.checked = true;
 }
 
-// Toggle theme on switch change
+//Toggle theme on switch change
 themeSwitch.addEventListener("change", () => {
     if (themeSwitch.checked) {
         body.classList.add("dark")
